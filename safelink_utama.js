9126fd281ec9d578ab651c7cdca0e8a86cdf2fe3
riooktapian <49826587+riooktapian@users.noreply.github.com>
2019-07-29 19:09:38 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -19,12 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="is-it-better-to-buy-or-lease-new-car.html"}
 if(x=="2"){xxx="the-multilayered-cryptocurrency.html"}
 if(x=="3"){xxx="5-simple-steps-to-earn-money-from.html"}
-if(x=="4"){xxx="5-digital-marketing-basics-you-should.html"}a_to_ve[i].href="https://safelink.maxoke.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="5-digital-marketing-basics-you-should.html"}
+if(x=="5"){xxx="best-free-blogger-templates-to-create.html"}
+if(x=="6"){xxx="7-cameras-that-offers-excellent-picture.html"}a_to_ve[i].href="https://safelink.maxoke.com/2019/07/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
